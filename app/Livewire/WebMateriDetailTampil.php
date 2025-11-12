e917c195833ae06c5b670dddfff9dbca7cc66223
Hagane <hargixia@gmail.com>
2025-11-12 18:26:50 +0700

    menambahkan view tampilan materi

--- app/Livewire/WebMateriDetailTampil.php
+++ app/Livewire/WebMateriDetailTampil.php
@@ -3,11 +3,23 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\data_materi_detail;
 
 class WebMateriDetailTampil extends Component
 {
+    public $id, $idmd;
+    public $materiNow;
+
+    public function mount(){
+        $this->materiNow = data_materi_detail::find($this->idmd)->get()->first();
+    }
+
     public function render()
     {
         return view('livewire.web-materi-detail-tampil');
+    }
+
+    public function kembali(){
+        return redirect('/materi/detail/'.$this->id);
     }
 }
--- resources/views/livewire/web-materi-detail-tampil.blade.php
+++ resources/views/livewire/web-materi-detail-tampil.blade.php
@@ -1,3 +1,39 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    {{ $materiNow->judul }}
+
+    {{ $materiNow->deskripsi }}
+
+    {{ $materiNow->tipe }}
+
+    @if($materiNow->tipe == 'teks')
+        <div class="mt-3">
+            {!! $materiNow->isi !!}
+        </div>
+    @elseif($materiNow->tipe == 'video')
+        <div class="mt-3">
+            <video width="100%" height="400" controls>
+                <source src="{{ asset('images/materi/' . $materiNow->isi) }}" type="video/mp4">
+            </video>
+        </div>
+    @elseif($materiNow->tipe == 'gambar')
+        <div class="mt-3 text-center">
+            <img src="{{ asset('images/materi/' . $materiNow->isi) }}" alt="{{ $materiNow->judul }}" class="img-fluid">
+        </div>
+    @elseif ($materiNow->tipe == 'audio')
+        <div class="mt-3">
+            <audio controls>
+                <source src="{{ asset('images/materi/' . $materiNow->isi) }}" type="audio/mpeg">
+            </audio>
+        </div>
+    @else
+        <div class="mt-3">
+            TIdak Ada Isi
+        </div>
+    @endif
+
+
+    <button type="button" wire:click='kembali' class="btn btn-secondary mt-4">
+        Kembali
+    </button>
+
 </div>
