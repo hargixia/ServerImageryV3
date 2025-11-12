9d1b0c17ba39b3f9aacd3c11b975b49cfc087301
Hagane <hargixia@gmail.com>
2025-11-12 16:13:43 +0700

    tambah materi detail tampil

---
+++ app/Livewire/WebMateriDetailTampil.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class WebMateriDetailTampil extends Component
+{
+    public function render()
+    {
+        return view('livewire.web-materi-detail-tampil');
+    }
+}
