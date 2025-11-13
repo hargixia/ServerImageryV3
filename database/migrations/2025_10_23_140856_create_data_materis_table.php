8f9ae5a6f0ff7b6052e5d4d62ff8c1227fc9a445
Hagane <hargixia@gmail.com>
2025-11-13 11:57:18 +0700

    penambhan tabel tugas

--- database/migrations/2025_10_23_140856_create_data_materis_table.php
+++ database/migrations/2025_10_23_140856_create_data_materis_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('judul');
             $table->text('deskripsi');
+            $table->boolean('tugas');
+            $table->boolean('exp');
+            $table->dateTime('start')->nullable();
+            $table->dateTime('stop')->nullable();
             $table->timestamps();
 
             $table->foreignId('id_apps')->constrained('data_apps','id');
---
+++ database/migrations/2025_11_13_030204_create_data_tugas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('data_tugas', function (Blueprint $table) {
+            $table->id();
+            $table->longText('isi');
+            $table->boolean('status');
+            $table->timestamps();
+
+            $table->foreignId('id_materi')->constrained('data_materis','id');
+            $table->foreignId('id_user')->constrained('users','id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('data_tugas');
+    }
+};
