c369f0d36aaf48eafebe88c8668efb6026de569c
Hagane <hargixia@gmail.com>
2025-11-16 04:15:02 +0700

    permintaan di perkembangan list

--- resources/views/livewire/web-perkembangan-list.blade.php
+++ resources/views/livewire/web-perkembangan-list.blade.php
@@ -1,8 +1,12 @@
 <div>
-    @foreach ($listUser as $lu)
+    @foreach ($listUser as $i => $lu)
         <div class="card m-3">
             <div class="card-body">
                 <p>{{$lu->nama}}</p>
+                <p>soal dikerjakan {{ $soal_dikerjakan[$i] }}</p>
+                <p>Rata2 = {{$rata2nilai[$i]}}</p>
+                <p>{{json_encode($performa[$i])}}</p>
+
                 <!-- Button trigger modal -->
                 <button type="button" class="btn btn-primary"  wire:click='tampilkan({{ $lu->id }})'>
                 Launch static backdrop modal
